d947691a708e98f9fa587e83c0521eeefd9b6c0c
XXXDESHXXX <kirya.shvars@list.ru>
2024-08-29 06:51:14 +0300

    Change

--- Docker/Apache_Conf/start.sh
+++ Docker/Apache_Conf/start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 sudo docker build -t custom-apache .
-sudo docker run --name custom-apache --network custom-network -p 8080:8080 -d custom-apache
+sudo docker run --name custom-apache --network custom-network -p 8083:8083 -d custom-apache
--- Docker/Nginx_Conf/Task12/start.sh
+++ Docker/Nginx_Conf/Task12/start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 sudo docker build -t custom-nginx .
-sudo docker run --name custom-nginx --network custom-network -p 443:443 -d custom-nginx
+sudo docker run --name custom-nginx --network custom-network -p 443:443 -p 80:80 -d custom-nginx
